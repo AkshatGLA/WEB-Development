dead17b2728741929141659a29c7aaa39369b5a8
AkshatGLA <tanishqsomani28@gmail.com>
2025-03-06 21:09:19 +0530

    Added new files

---
+++ JS/array.js
@@ -0,0 +1,21 @@
+arr=["akshat","tanishq","somani"];
+
+console.log(arr);
+
+console.log(arr[1]);
+
+console.log(arr.length);
+
+arr[10]="akshatsomani";
+console.log(arr.length);
+console.log(arr);
+
+// push: add element at the end of array
+// pop: remove element from the end of array
+// shift: remove element from the start of array
+// unshift: add element at the start of array
+// splice: remove element from the middle of array
+// slice: copy array
+// concat: merge two arrays
+// indexOf: find index of element
+// includes: check if element is present in array
---
+++ auto_push.ps1
@@ -0,0 +1,4 @@
+cd "C:\Users\Acer\Desktop\akshat somani\gla akshat somani\WEBD"
+git add .
+git commit -m "Auto-update on VS Code open"
+git push origin main
